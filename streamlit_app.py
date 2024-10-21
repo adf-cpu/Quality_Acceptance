587d542765e848b7a834e7083415d79974b9d105
adf-cpu <kinza.tanveer2@h-partners.com>
2024-10-21 10:55:57 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,8 @@ import os
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
+    .st-emotion-cache-1huvf7z {
+        display: none; /* Hides the button */
     }
     </style>
     """,
